85677261aad1dd74cbc5e069563e743daac50362
adamgit <adam@adams-MacBook-Air.local>
2013-10-14 13:27:46 +0100

    (Part 4): fixed the glCheckErrors code to use macros correctly (thanks to Chris Ross)

--- GLK2/GLK2/GLK2GetError.h
+++ GLK2/GLK2/GLK2GetError.h
@@ -3,10 +3,13 @@
  */
 
 #import <Foundation/Foundation.h>
+#include <stdio.h>
 
 #ifndef GLK2_GLK2GetError_h
 #define GLK2_GLK2GetError_h
 
-void gl2CheckAndClearAllErrors();
+void _gl2CheckAndClearAllErrorsImpl(const char *source_function, const char *source_file, int source_line);
+
+#define gl2CheckAndClearAllErrors() _gl2CheckAndClearAllErrorsImpl(__PRETTY_FUNCTION__,__FILE__,__LINE__)
 
 #endif
--- GLK2/GLK2/GLK2GetError.m
+++ GLK2/GLK2/GLK2GetError.m
@@ -10,7 +10,7 @@
 #include "GLK2GetError.h"
 #include <GLKit/GLKit.h>
 
-void gl2CheckAndClearAllErrors()
+void _gl2CheckAndClearAllErrorsImpl(const char *source_function, const char *source_file, int source_line)
 {
 	GLenum glErrorLast;
 	while( (glErrorLast = glGetError()) != GL_NO_ERROR ) // GL spec says you must do this in a WHILE loop
@@ -18,7 +18,7 @@ void gl2CheckAndClearAllErrors()
 		/** OpenGL spec defines only 6 legal errors, that HAVE to be re-used by all gl method calls. OH THE PAIN! */
 		NSDictionary* glErrorNames = @{ @(GL_INVALID_ENUM) : @"GL_INVALID_ENUM", @(GL_INVALID_VALUE) : @"GL_INVALID_VALUE", @(GL_INVALID_OPERATION) : @"GL_INVALID_OPERATION", @(GL_STACK_OVERFLOW) : @"GL_STACK_OVERFLOW", @(GL_STACK_UNDERFLOW) : @"GL_STACK_UNDERFLOW", @(GL_OUT_OF_MEMORY) : @"GL_OUT_OF_MEMORY" };
 		
-		NSLog(@"GL Error: %@ in %s @ %s:%d", [glErrorNames objectForKey:@(glErrorLast)], __PRETTY_FUNCTION__, __FILE__, __LINE__ );
+		NSLog(@"GL Error: %@ in %s @ %s:%d", [glErrorNames objectForKey:@(glErrorLast)], source_function, source_file, source_line );
 		
 		NSCAssert( FALSE, @"OpenGL Error; you need to investigate this!" ); // can't use NSAssert, because we're inside a C function
 	}
