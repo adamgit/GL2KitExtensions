9c671fda2895ad2d25e578b2b9b87a74fc166cbd
adamgit <adam.m.s.martin@gmail.com>
2014-03-12 20:43:01 +0000

    ADDED: deliberately annoying warning when there's a nil drawcall.uniformValueGenerator

--- GLKX-Library/GLKX-Library/Essential OpenGL classes/GLK2DrawCall.m
+++ GLKX-Library/GLKX-Library/Essential OpenGL classes/GLK2DrawCall.m
@@ -137,7 +137,14 @@
 
 -(void) setAllUniformValuesForShader
 {
-	if( self.uniformValueGenerator != nil )
+	if( self.uniformValueGenerator == nil )
+	{
+		if( [self.shaderProgram allUniforms].count > 0 )
+		{
+			NSLog(@"WARNING: this DrawCall has uniforms, but no uniformValueGenerator; if another object ever uses the same ShaderProgram, and changes the values, this DrawCall will get 'leaked' incorrect values. Set a .uniformValueGenerator to remove this message - even an empty one that returns false for everything.");
+		}
+	}
+	else
 	{
 		for( GLK2Uniform* uniform in [self.shaderProgram allUniforms] )
 		{			
